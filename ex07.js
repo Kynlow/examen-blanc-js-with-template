fe5f3de4c659569fd640448fa7deb7ebd3319add
Kynlow <elwen@areastudio.fr>
2025-11-04 15:22:18 +0100

    zfez

--- ex07.js
+++ ex07.js
@@ -7,7 +7,7 @@ function getReduction(price, age) {
 	if (age <= 25 && age > 18) {
 		return price - price * 0.3;
 	}
-	if (age <= 18 && age > 5) {
+	if (age <= 18 && age >= 5) {
 		return price - price * 0.5;
 	}
 	if (age < 5) {
